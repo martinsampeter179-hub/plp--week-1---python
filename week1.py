2016184ddf24fad5c9c92d2201d4ad5aed301665
martinsampeter179-hub <martinsampeter179@gmail.com>
2026-09-14 12:40:41 +0300

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,15 @@
+# my first python program name, age,goal and favorite language
+name ="martin"
+age = 25
+goal = "learn python"
+favorite_language = "Python"
+
+# let's print the results
+print(f"my name is {name}")
+print(f"my age is {age}")
+print(f"my goal is to {goal}")
+print(f"my favorite programming language is {favorite_language}")
+
+# variable for introduction
+introduction = "hello, my name is martin a plp student" 
+print(f"introduction")
